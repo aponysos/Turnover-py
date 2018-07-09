9d25d32aa4b05a1ea0c30566341c82e2f4785059
aponysos <aponysos@163.com>
2018-07-09 18:11:30 +0800

    *init_squares: +global

--- Turnover.py
+++ Turnover.py
@@ -7,39 +7,47 @@ rate = 0.4
 
 ST_NONE = 0
 ST_BLACK = 1
-MOVES = [(-1, 0), (0, -1), (1, 0), (0, 1)]
-SZ = 20
+MOVES = [(-1, 0), (0, -1), (1, 0), (0, 1)]  # left, up, right, down
+SQUARE_SIZE = 20
 PADDING = 4
 
-class Turnover(cocos.layer.Layer):
-    squares = []
-    selected = []
-    cur = (0, 0)
+################################################################################################
 
+
+class Turnover(cocos.layer.Layer):
     def __init__(self):
         super(Turnover, self).__init__()
 
-        self.init_squares()
+        init_squares()
 
-        label = cocos.text.Label( \
-            '\n'.join([str(row) for row in self.squares]), \
-            font_name='Times New Roman', \
-            font_size=32, \
-            anchor_x='center', anchor_y='center', \
-            multiline=True, width=240 \
+        label = cocos.text.Label(
+            '\n'.join([str(row) for row in squares]),
+            font_name='Times New Roman',
+            font_size=32,
+            anchor_x='center', anchor_y='center',
+            multiline=True, width=240
         )
         label.position = 320, 240
         self.add(label)
 
-    def init_squares(self):
-        self.squares = [[ \
-            ST_BLACK if (not self.is_on_boundary(row, col) and random.random() < rate) else ST_NONE \
-            for col in range(max_col)] for row in range(max_row)]
-
-    def is_on_boundary(self, row, col):
-        return row == 0 or col == 0 or row == max_row-1 or col == max_col-1
+################################################################################################
 
 
+def init_squares():
+    global squares, selected, cur
+    squares = [[
+        ST_BLACK if (not is_on_boundary(col, row)
+                     and random.random() < rate) else ST_NONE
+        for col in range(max_col)] for row in range(max_row)]
+    selected = []
+    cur = (0, 0)
+
+
+def is_on_boundary(col, row):
+    return col == 0 or col == max_col-1 or row == 0 or row == max_row-1
+
+
+################################################################################################
 cocos.director.director.init()
 turnover_layer = Turnover()
 main_scene = cocos.scene.Scene(turnover_layer)
